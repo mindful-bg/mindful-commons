858efc4b0c905c654dde2965c68ff2b3619f552c
wideyedwonderer <codisanko@gmail.com>
2021-02-07 13:12:37 +0200

    Auto-prepublish-commit

--- types/blog/get-blog-filter.dto.ts
+++ types/blog/get-blog-filter.dto.ts
@@ -1,7 +1,29 @@
-import { Allow } from "class-validator";
-import { GetContentFilter } from "../content";
+import { Allow, IsString } from "class-validator";
+import { ContentType, GetContentFilter } from "../content";
+import { MindfulAspects } from "../content/mindful-aspects.enum";
 
 export class GetBlogFilter extends GetContentFilter {
 
+    @Allow()
     categoryId: string;
+    @Allow()
+    id?: string;
+    @Allow()
+    mindfulAspect?: MindfulAspects;
+    @Allow()
+    dateCreated?: number;
+    @Allow()
+    tags?: string[];
+    @Allow()
+    isDraft?: boolean;
+    @Allow()
+    isPublished?: boolean;
+    @Allow()
+    type?: ContentType;
+    @IsString()
+    creatorId?: string;
+    @Allow()
+    limit?: number;
+    @Allow()
+    skip?: number;
 }
