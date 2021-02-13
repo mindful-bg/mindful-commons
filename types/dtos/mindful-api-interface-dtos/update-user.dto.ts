4419145eb187887bb068b6378fc845c26ea40a33
wideyedwonderer <codisanko@gmail.com>
2021-02-13 17:23:40 +0200

    Auto-prepublish-commit

--- types/dtos/mindful-api-interface-dtos/update-user.dto.ts
+++ types/dtos/mindful-api-interface-dtos/update-user.dto.ts
@@ -2,26 +2,20 @@ import { MaxLength, Allow } from "class-validator";
 
 export class UserUpdateDto {
     @Allow()
-    @MaxLength(100)
     firstName: string;
 
     @Allow()
-    @MaxLength(100)
     lastName: string;
 
     @Allow()
-    @MaxLength(100)
     contactEmail?: string;
 
     @Allow()
-    @MaxLength(30)
     birthDate?: string;
 
     @Allow()
-    @MaxLength(50)
     location?: string;
 
     @Allow()
-    @MaxLength(20)
     shortName?: string;
 }
