0a1ea2c52b1442b4e6dd1604c2a1eea84804867e
wideyedwonderer <codisanko@gmail.com>
2021-02-20 14:14:24 +0200

    export regex

--- constants/regex.ts
+++ constants/regex.ts
@@ -1,6 +1,6 @@
 export const MindfulRegex = {
     password: {
         errorMessage: 'Паролата ви трябва да съдържа поне 8 символа, една главна буква и една цифра',
-        regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])$/  
+        regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/  
     }
 }
