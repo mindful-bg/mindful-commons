83b584bc29328cd408705990b331c105d60a13a1
wideyedwonderer <codisanko@gmail.com>
2021-03-13 16:55:58 +0200

    Auto-prepublish-commit

--- types/dtos/mindful-api-interface-dtos/get-publishers.dto.ts
+++ types/dtos/mindful-api-interface-dtos/get-publishers.dto.ts
@@ -1,8 +1,16 @@
+import { Expose } from "class-transformer";
+
 export class GetPublishersDto {
+    @Expose()
     id?: string;
+    @Expose()
     firstName?: string;
+    @Expose()
     lastName?: string;
+    @Expose()
     shortName?: string;
+    @Expose()
     slug?: string;
+    @Expose()
     personalStoryContentId?: string;
 }
