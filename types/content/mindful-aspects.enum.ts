5174bf60e350eb222710619c3ef0d0be714c9af9
wideyedwonderer <codisanko@gmail.com>
2021-03-21 10:58:33 +0200

    Auto-prepublish-commit

--- types/content/mindful-aspects.enum.ts
+++ types/content/mindful-aspects.enum.ts
@@ -1,7 +1,27 @@
 export enum MindfulAspects {
-    ENVIRONMENT = 'Заобикалящ свят',
-    BODY_AND_EMOTION = 'Тяло и емоции',
-    THOUGHTS = 'Мисли',
-    RELATIONSHIPS = 'Взаимоотношения',
-    SPIRITUALITY = 'Духовност'
+    ENVIRONMENT = 'zaobikalyasht-svyat',
+    BODY_AND_EMOTION = 'tyalo-i-emocii',
+    THOUGHTS = 'misli',
+    RELATIONSHIPS = 'vzaimootnosheniya',
+    SPIRITUALITY = 'duhovnost'
+}
+
+export const getMindfulAspectViewValue = (aspect: MindfulAspects) => {
+    switch(aspect) {
+        case(MindfulAspects.ENVIRONMENT): {
+            return 'Заобикалящ свят'
+        }
+        case(MindfulAspects.BODY_AND_EMOTION): {
+            return 'Тяло и емоции'
+        }
+        case(MindfulAspects.THOUGHTS): {
+            return 'Мисли'
+        }
+        case(MindfulAspects.RELATIONSHIPS): {
+            return 'Взаимоотношения'
+        }
+        case(MindfulAspects.SPIRITUALITY): {
+            return 'Духовност'
+        }
+    }
 }
