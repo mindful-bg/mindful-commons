7ccc56668ab037c4755aa5ceb9ee0fe7ff63fb2b
wideyedwonderer <codisanko@gmail.com>
2021-03-21 16:04:10 +0200

    Auto-prepublish-commit

--- types/dtos/mindful-api-interface-dtos/create-category.dto.ts
+++ types/dtos/mindful-api-interface-dtos/create-category.dto.ts
@@ -1,6 +1,9 @@
-import { IsString } from "class-validator";
+import { IsString, Matches, MaxLength } from "class-validator";
 
 export class CreateCategoryDto {
     @IsString()
     name: string;
+    @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {message: "Невалиден формат на слъг-а. Използвайте латински букви, числа и тирета(-)."})
+    @MaxLength(45, {message: "Слъг-а трябва да е от максимум $constraint1 символа."})
+    slug: string;
 }
