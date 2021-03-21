a3927c09be9bf9fcfef206d711526a620e13e94a
wideyedwonderer <codisanko@gmail.com>
2021-03-21 17:06:56 +0200

    Auto-prepublish-commit

--- types/dtos/mindful-api-interface-dtos/rename-category.dto.ts
+++ types/dtos/mindful-api-interface-dtos/rename-category.dto.ts
@@ -1,8 +1,11 @@
-import { IsString } from "class-validator";
+import { IsString, Matches, MaxLength } from "class-validator";
 
 export class RenameCategoryDto {
     @IsString()
     id: string;
     @IsString()
     newName: string;
+    @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {message: "Невалиден формат на слъг-а. Използвайте латински букви, числа и тирета(-)."})
+    @MaxLength(45, {message: "Слъг-а трябва да е от максимум $constraint1 символа."})
+    newSlug: string;
 }
