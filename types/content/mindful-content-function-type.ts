6020bb6febd7ab61d24dd0487eec6d462a931149
wideyedwonderer <codisanko@gmail.com>
2021-03-21 11:05:56 +0200

    Auto-prepublish-commit

--- types/content/mindful-content-function-type.ts
+++ types/content/mindful-content-function-type.ts
@@ -1,4 +1,15 @@
 export enum MindfulContentFunctionType {
-    CONNECTING = 'Свързващ',
-    STRUCTURING = 'Структуриращ'
+    CONNECTING = 'svyrzvasht',
+    STRUCTURING = 'strukturirasht'
+}
+
+export const getMindfulFunctionViewValue = (funciton: MindfulContentFunctionType) => {
+    switch(funciton) {
+        case(MindfulContentFunctionType.CONNECTING): {
+            return 'Свързващ'
+        }
+        case(MindfulContentFunctionType.STRUCTURING): {
+            return 'Структуриращ'
+        }
+    }
 }
