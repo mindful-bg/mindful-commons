372ab8fe28eb9b2d1b60d9d7e3446d944c5aaf9b
wideyedwonderer <codisanko@gmail.com>
2021-04-25 11:00:55 +0300

    Auto-prepublish-commit

--- types/dtos/mindful-api-interface-dtos/update-publisher.dto.ts
+++ types/dtos/mindful-api-interface-dtos/update-publisher.dto.ts
@@ -1,8 +1,9 @@
-import { IsOptional, IsString, Matches, MaxLength } from "class-validator";
+import { IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class UpdatePublisherDto {
     @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {message: "Невалиден формат на слъг-а. Използвайте латински букви, числа и тирета(-)."})
-    @MaxLength(45, {message: "Слъг-а трябва да е от максимум $constraint1 символа."})
+    @MaxLength(20, {message: "Слъг-а трябва да е от максимум $constraint1 символа."})
+    @MinLength(3, {message: "Слъг-а трябва да е от минимум $constraint1 символа."})
     slug: string;
 
     @MaxLength(30)
@@ -13,7 +14,8 @@ export class UpdatePublisherDto {
     @IsOptional()
     quote?: string;
 
-    @MaxLength(20)
+    @MaxLength(20, {message: "Краткото име трябва да е от максимум $constraint1 символа."})
+    @MinLength(3, {message: "Краткото име трябва да е от минимум $constraint1 символа."})
     @IsString()
     shortName?: string;
 
