92113a881f7ea9628eecb6b9b704c8a36cd413dd
wideyedwonderer <codisanko@gmail.com>
2021-04-25 13:17:08 +0300

    Auto-prepublish-commit

--- types/dtos/mindful-api-interface-dtos/reset-password.dto.ts
+++ types/dtos/mindful-api-interface-dtos/reset-password.dto.ts
@@ -2,9 +2,6 @@ import { Allow, IsNotEmpty, Matches } from "class-validator";
 import { MindfulRegex } from "../../../constants";
 
 export class ResetPasswordDto {
-    @IsNotEmpty()
-    readonly email: string;
-    
     @Matches(MindfulRegex.password.regex, {message: MindfulRegex.password.errorMessage})
     readonly newPassword: string;
 
