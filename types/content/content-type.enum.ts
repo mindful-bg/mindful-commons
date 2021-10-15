c81c1029f21307bcf4f3c084b5c7253736e9950d
wideyedwonderer <codisanko@gmail.com>
2021-10-15 16:04:24 +0300

    Auto-prepublish-commit

--- types/content/content-type.enum.ts
+++ types/content/content-type.enum.ts
@@ -3,7 +3,7 @@ export enum ContentType {
     EXERCISE = "ПРАКТИКА",
     USER_STORY = "ЛИЧНА ИСТОРИЯ",
     PERSONAL_PAGE = "ЛИЧНА СТРАНИЦА",
-    MINDFUL_BG_PAGE = "СПЕЦИАЛНА СТРАНИЦА НА САЙТА",
     PUBLISHERS_RESOURCE = "РЕСУРС НА ПУБЛИКУВАЩИЯ",
-    SERIES = "ПОРЕДИЦА"
+    BLOG_SERIES = "БЛОГ ПОРЕДИЦА",
+    THEORY_SERIES = "ТЕОРЕТИЧНА ПОРЕДИЦА"
 }
