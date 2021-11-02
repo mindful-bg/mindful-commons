233fa1dbebb5cf177799eac8df310111c309edaf
wideyedwonderer <codisanko@gmail.com>
2021-11-02 16:34:37 +0200

    Auto-prepublish-commit

--- types/content/get-series.dto.ts
+++ types/content/get-series.dto.ts
@@ -3,5 +3,5 @@ import { GetContentDto } from "./get-content.dto";
 import { Series } from "./series.entity";
 
 export class GetSeriesDto extends Series {
-    content: GetContentDto | GetBlogEntriesDto;
+    content: GetContentDto;
 }
